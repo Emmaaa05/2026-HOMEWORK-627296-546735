68ea9fc736b10deb687b09bff3aedf51c46644ef
Osayi Lois <osayilois18@gmail.com>
2026-04-07 23:35:40 +0200

    Progetto (refactor): Giocatore e Borsa creati. Partita rifattorizzata

---
+++ src/it/uniroma3/diadia/Giocatore.java
@@ -0,0 +1,30 @@
+package it.uniroma3.diadia;
+
+public class Giocatore {
+	
+	
+	private int cfu;
+	private Borsa borsa;
+	
+	/* costruttore di Giocatore */
+	public Giocatore(int CFU_INIZIALI) {
+		this.cfu = CFU_INIZIALI;
+		this.borsa = new Borsa();
+		
+	}
+	
+	/* getter e setter */
+	public int getCfu() {
+		return this.cfu;
+	}
+	
+	public void setCfu(int cfu) {
+		this.cfu = cfu;
+	}
+	
+	public Borsa getBorsa() {
+		return this.borsa;
+		
+	}
+
+}
